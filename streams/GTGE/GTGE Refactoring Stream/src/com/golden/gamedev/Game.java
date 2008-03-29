f8a34a3641183de632a7e199354409c8bce4d011
MetroidFan2002 <MetroidFan2002@7e9cb074-ce48-0410-a09f-2fa9ee36210d>
2008-03-29 20:25:51 +0000

    All the final methods common to GameObject and Game have been "pulled up".  BaseGame will now be reorganized a little bit for the next commit.

--- streams/GTGE/GTGE Refactoring Stream/src/com/golden/gamedev/Game.java
+++ streams/GTGE/GTGE Refactoring Stream/src/com/golden/gamedev/Game.java
@@ -51,7 +51,6 @@ import com.golden.gamedev.object.PlayField;
 import com.golden.gamedev.object.Sprite;
 import com.golden.gamedev.object.SpriteGroup;
 import com.golden.gamedev.util.ImageUtil;
-import com.golden.gamedev.util.Utility;
 
 /**
  * <code>Game</code> class is <b>Golden T Game Engine (GTGE) core class</b>
@@ -627,22 +626,6 @@ public abstract class Game extends BaseGame {
 	/** ************************************************************************* */
 	
 	/**
-	 * All game resources initialization, everything that usually goes to
-	 * constructor should be put in here.
-	 * <p>
-	 * 
-	 * This method is called only once for every newly created <code>Game</code>
-	 * class.
-	 * 
-	 * @see #getImage(String)
-	 * @see #getImages(String, int, int)
-	 * @see #playMusic(String)
-	 * @see #setMaskColor(Color)
-	 * @see com.golden.gamedev.object
-	 */
-	public abstract void initResources();
-	
-	/**
 	 * Notified when the game is about to quit. By default this method is
 	 * calling <code>System.exit()</code> to ensure everything is properly
 	 * shut down.
@@ -1083,144 +1066,6 @@ public abstract class Game extends BaseGame {
 	/** ***************** (PASTE INTO GAME OBJECT CLASS) ******************** */
 	/** ************************************************************************* */
 	
-	/** ************************************************************************* */
-	/** *********************** ESSENTIAL GAME UTILITY ************************** */
-	/** ************************************************************************* */
-	// -> com.golden.gamedev.util.Utility
-	/**
-	 * Effectively equivalent to the call
-	 * {@linkplain com.golden.gamedev.util.Utility#getRandom(int, int)
-	 * Utility.getRandom(int, int)}
-	 */
-	public int getRandom(int low, int hi) {
-		return Utility.getRandom(low, hi);
-	}
-	
-	// INTERNATIONALIZATION UTILITY
-	// public Locale getLocale() { return locale; }
-	// public void setLocale(Locale locale) { this.locale = locale; }
-	
-	/** ************************************************************************* */
-	/** ************************** AUDIO UTILITY ******************************** */
-	/** ************************************************************************* */
-	// -> com.golden.gamedev.engine.BaseAudio
-	/**
-	 * Effectively equivalent to the call
-	 * {@linkplain com.golden.gamedev.engine.BaseAudio#play(String)
-	 * bsMusic.play(String)}.
-	 * 
-	 * @see com.golden.gamedev.engine.BaseAudio#setBaseRenderer(com.golden.gamedev.engine.BaseAudioRenderer)
-	 * @see com.golden.gamedev.engine.audio
-	 */
-	public int playMusic(String audiofile) {
-		return this.bsMusic.play(audiofile);
-	}
-	
-	/**
-	 * Effectively equivalent to the call
-	 * {@linkplain com.golden.gamedev.engine.BaseAudio#play(String)
-	 * bsSound.play(String)}.
-	 * 
-	 * @see com.golden.gamedev.engine.BaseAudio#setBaseRenderer(com.golden.gamedev.engine.BaseAudioRenderer)
-	 * @see com.golden.gamedev.engine.audio
-	 */
-	public int playSound(String audiofile) {
-		return this.bsSound.play(audiofile);
-	}
-	
-	/**
-	 * Effectively equivalent to the call
-	 * {@linkplain com.golden.gamedev.engine.BaseTimer#getCurrentFPS()
-	 * bsTimer.getCurrentFPS()}.
-	 */
-	public int getCurrentFPS() {
-		return this.bsTimer.getCurrentFPS();
-	}
-	
-	/**
-	 * Effectively equivalent to the call
-	 * {@linkplain com.golden.gamedev.engine.BaseTimer#getFPS()}.
-	 */
-	public int getFPS() {
-		return this.bsTimer.getFPS();
-	}
-	
-	/**
-	 * Draws game frame-per-second (FPS) to specified location.
-	 */
-	public void drawFPS(Graphics2D g, int x, int y) {
-		this.fontManager.getFont("FPS Font").drawString(g,
-		        "FPS = " + this.getCurrentFPS() + "/" + this.getFPS(), x, y);
-	}
-	
-	/** ************************************************************************* */
-	/** ************************** INPUT UTILITY ******************************** */
-	/** ************************************************************************* */
-	// -> com.golden.gamedev.engine.BaseInput
-	/**
-	 * Effectively equivalent to the call
-	 * {@linkplain com.golden.gamedev.engine.BaseInput#getMouseX()
-	 * bsInput.getMouseX()}.
-	 */
-	public int getMouseX() {
-		return this.bsInput.getMouseX();
-	}
-	
-	/**
-	 * Effectively equivalent to the call
-	 * {@linkplain com.golden.gamedev.engine.BaseInput#getMouseY()
-	 * bsInput.getMouseY()}.
-	 */
-	public int getMouseY() {
-		return this.bsInput.getMouseY();
-	}
-	
-	/**
-	 * Returns whether the mouse pointer is inside specified screen boundary.
-	 */
-	public boolean checkPosMouse(int x1, int y1, int x2, int y2) {
-		return (this.getMouseX() >= x1 && this.getMouseY() >= y1
-		        && this.getMouseX() <= x2 && this.getMouseY() <= y2);
-	}
-	
-	/**
-	 * Returns whether the mouse pointer is inside specified sprite boundary.
-	 * 
-	 * @param sprite sprite to check its intersection with mouse pointer
-	 * @param pixelCheck true, checking the sprite image with pixel precision
-	 */
-	public boolean checkPosMouse(Sprite sprite, boolean pixelCheck) {
-		Background bg = sprite.getBackground();
-		
-		// check whether the mouse is in background clip area
-		if (this.getMouseX() < bg.getClip().x
-		        || this.getMouseY() < bg.getClip().y
-		        || this.getMouseX() > bg.getClip().x + bg.getClip().width
-		        || this.getMouseY() > bg.getClip().y + bg.getClip().height) {
-			return false;
-		}
-		
-		double mosx = this.getMouseX() + bg.getX() - bg.getClip().x;
-		double mosy = this.getMouseY() + bg.getY() - bg.getClip().y;
-		
-		if (pixelCheck) {
-			try {
-				return ((sprite.getImage().getRGB((int) (mosx - sprite.getX()),
-				        (int) (mosy - sprite.getY())) & 0xFF000000) != 0x00);
-			}
-			catch (Exception e) {
-				return false;
-			}
-			
-		}
-		else {
-			return (mosx >= sprite.getX() && mosy >= sprite.getY()
-			        && mosx <= sprite.getX() + sprite.getWidth() && mosy <= sprite
-			        .getY()
-			        + sprite.getHeight());
-		}
-	}
-	
 	/**
 	 * Returns sprite in specified sprite group that intersected with mouse
 	 * pointer, or null if no sprite intersected with mouse pointer.
@@ -1263,24 +1108,6 @@ public abstract class Game extends BaseGame {
 		}
 		
 		return null;
-	}
-	
-	/**
-	 * Effectively equivalent to the call
-	 * {@linkplain com.golden.gamedev.engine.BaseInput#isMousePressed(int)
-	 * bsInput.isMousePressed(java.awt.event.MouseEvent.BUTTON1)}.
-	 */
-	public boolean click() {
-		return this.bsInput.isMousePressed(MouseEvent.BUTTON1);
-	}
-	
-	/**
-	 * Effectively equivalent to the call
-	 * {@linkplain com.golden.gamedev.engine.BaseInput#setMouseVisible(boolean)
-	 * bsInput.setMouseVisible(false)}.
-	 */
-	public void hideCursor() {
-		this.bsInput.setMouseVisible(false);
 	}
 	
 }

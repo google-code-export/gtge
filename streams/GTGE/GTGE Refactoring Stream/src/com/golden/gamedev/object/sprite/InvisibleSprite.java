5ce6af3df863604bab13ff914411f74e980650ae
MetroidFan2002 <MetroidFan2002@7e9cb074-ce48-0410-a09f-2fa9ee36210d>
2008-09-06 22:06:25 +0000

    Modified the InvisibleSprite class to add the constructors of the Sprite class and to enable an InvisibleSprite to change from invisible to visible.  By default, an InvisibleSprite will still be invisible when constructed.

--- streams/GTGE/GTGE Refactoring Stream/src/com/golden/gamedev/object/sprite/InvisibleSprite.java
+++ streams/GTGE/GTGE Refactoring Stream/src/com/golden/gamedev/object/sprite/InvisibleSprite.java
@@ -18,23 +18,107 @@ package com.golden.gamedev.object.sprite;
 
 // JFC
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.Serializable;
 
 import com.golden.gamedev.object.Sprite;
 
 /**
- * <code>InvisibleSprite</code> is sprite that has no graphical image,
- * generally used in collision to make invisible block.
+ * The <tt>InvisibleSprite</tt> class is a {@link Sprite} that will not render
+ * its {@link #getImage() image} by default, but is useful for simulating a
+ * collision against an invisible block. However, the
+ * {@link #setVisible(boolean)} method may now be invoked to re-enable the
+ * rendering of the <tt>InvisibleSprite</tt> instance, for example, when a power up block is
+ * hit, the power up block then displays its image and is no longer invisible to
+ * the user.
+ * 
+ * @since 0.2.3
+ * @version 1.1
  */
 public class InvisibleSprite extends Sprite {
 	
 	/**
-	 * 
+	 * A serialVersionUID for the <tt>InvisibleSprite</tt> class.
+	 * @see Serializable
 	 */
 	private static final long serialVersionUID = 5126744876218108841L;
 	
 	/**
-	 * Creates new <code>InvisibleSprite</code> at specified position and
-	 * specified size.
+	 * Whether or not this <tt>InvisibleSprite</tt> instance is visible, that
+	 * is, whether or not it will {@link #render(Graphics2D) render} to the
+	 * {@link Graphics2D graphics instance}.
+	 * @see #render(Graphics2D)
+	 * @see #render(Graphics2D, int, int)
+	 */
+	private boolean visible = false;
+	
+	/**
+	 * Creates a new <tt>InvisibleSprite</tt> instance with the
+	 * {@link Sprite#Sprite()} constructor, set to be invisible by default.
+	 */
+	public InvisibleSprite() {
+		super();
+	}
+	
+	/**
+	 * Creates a new <tt>InvisibleSprite</tt> instance with the
+	 * {@link Sprite#Sprite(BufferedImage)} constructor, set to be invisible by
+	 * default.
+	 * @param image The {@link #getImage() image} of this
+	 *        <tt>InvisibleSprite</tt> to render if
+	 *        {@link #setVisible(boolean) set to be visible}.
+	 */
+	public InvisibleSprite(BufferedImage image) {
+		super(image);
+	}
+	
+	/**
+	 * Creates a new <tt>InvisibleSprite</tt> instance with the
+	 * {@link Sprite#Sprite(BufferedImage, double, double)} constructor, set to
+	 * be invisible by default.
+	 * @param image The {@link #getImage() image} of this
+	 *        <tt>InvisibleSprite</tt> to render if
+	 *        {@link #setVisible(boolean) set to be visible}.
+	 * @param x The {@link #getX() x-coordinate position} of this
+	 *        <tt>InvisibleSprite</tt> instance.
+	 * @param y The {@link #getY() y-coordinate position} of this
+	 *        <tt>InvisibleSprite</tt> instance.
+	 */
+	public InvisibleSprite(BufferedImage image, double x, double y) {
+		super(image, x, y);
+	}
+	
+	/**
+	 * Creates a new <tt>InvisibleSprite</tt> instance with the
+	 * {@link Sprite#Sprite(double, double)} constructor, set to be invisible by
+	 * default.
+	 * @param x The {@link #getX() x-coordinate position} of this
+	 *        <tt>InvisibleSprite</tt> instance.
+	 * @param y The {@link #getY() y-coordinate position} of this
+	 *        <tt>InvisibleSprite</tt> instance.
+	 */
+	public InvisibleSprite(double x, double y) {
+		super(x, y);
+	}
+	
+	/**
+	 * Creates a new <tt>InvisibleSprite</tt> instance with the specified
+	 * position and size, set to be invisible by default. This
+	 * <tt>InvisibleSprite</tt> instance will not be associated with an
+	 * {@link #getImage() image} unless the
+	 * {@link #setImage(java.awt.image.BufferedImage)} method is invoked after
+	 * construction. It is generally recommended that this constructor is only
+	 * used when the <tt>InvisibleSprite</tt> instance will never have an
+	 * image to render, otherwise, one of the other constructors of the
+	 * <tt>InvisibleSprite</tt> class should be invoked instead.
+	 * @param x The {@link #getX() x-coordinate position} of this
+	 *        <tt>InvisibleSprite</tt> instance.
+	 * @param y The {@link #getY() y-coordinate position} of this
+	 *        <tt>InvisibleSprite</tt> instance.
+	 * @param width The {@link #getWidth() width} of this
+	 *        <tt>InvisibleSprite</tt> instance.
+	 * @param height The {@link #getHeight() height} of this
+	 *        <tt>InvisibleSprite</tt> instance.
 	 */
 	public InvisibleSprite(double x, double y, int width, int height) {
 		super(x, y);
@@ -43,16 +127,45 @@ public class InvisibleSprite extends Sprite {
 		this.height = height;
 	}
 	
-	/**
-	 * Renders nothing, the implementation of this method is blank.
-	 */
 	public void render(Graphics2D g) {
+		if (isVisible()) {
+			super.render(g);
+		}
+	}
+	
+	public void render(Graphics2D g, int x, int y) {
+		if (isVisible()) {
+			super.render(g, x, y);
+		}
 	}
 	
 	/**
-	 * Renders nothing, the implementation of this method is blank.
+	 * Gets whether or not this <tt>InvisibleSprite</tt> instance is visible,
+	 * that is, whether or not it will {@link #render(Graphics2D) render} to the
+	 * {@link Graphics2D graphics instance}.
+	 * @return Whether or not this <tt>InvisibleSprite</tt> instance is
+	 *         visible, that is, whether or not it will
+	 *         {@link #render(Graphics2D) render} to the
+	 *         {@link Graphics2D graphics instance}.
+	 * @see #render(Graphics2D)
+	 * @see #render(Graphics2D, int, int)
 	 */
-	public void render(Graphics2D g, int x, int y) {
+	public boolean isVisible() {
+		return visible;
 	}
 	
+	/**
+	 * Sets whether or not this <tt>InvisibleSprite</tt> instance is visible,
+	 * that is, whether or not it will {@link #render(Graphics2D) render} to the
+	 * {@link Graphics2D graphics instance}.
+	 * @param visible Whether or not this <tt>InvisibleSprite</tt> instance is
+	 *        visible, that is, whether or not it will
+	 *        {@link #render(Graphics2D) render} to the
+	 *        {@link Graphics2D graphics instance}.
+	 * @see #render(Graphics2D)
+	 * @see #render(Graphics2D, int, int)
+	 */
+	public void setVisible(boolean visible) {
+		this.visible = visible;
+	}
 }

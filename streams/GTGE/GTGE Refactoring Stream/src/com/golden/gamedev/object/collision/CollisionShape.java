82d4dab728529d48446ab11afbcbb8f0c78ad270
MetroidFan2002 <MetroidFan2002@7e9cb074-ce48-0410-a09f-2fa9ee36210d>
2008-09-09 02:42:48 +0000

    Committed changes to comments, added TODO comments for further evaluation and refactoring.

--- streams/GTGE/GTGE Refactoring Stream/src/com/golden/gamedev/object/collision/CollisionShape.java
+++ streams/GTGE/GTGE Refactoring Stream/src/com/golden/gamedev/object/collision/CollisionShape.java
@@ -16,57 +16,104 @@
  */
 package com.golden.gamedev.object.collision;
 
+import com.golden.gamedev.object.Sprite;
+
 /**
- * <code>CollisionShape</code> interface represents a form of geometric shape
- * that behave as sprite collision area. <code>CollisionShape</code> is able
- * to determine whether its area is intersected with another collision shape
- * area.
- * <p>
+ * The <tt>CollisionShape</tt> interface represents a "bounding box" that
+ * behaves as a collision area for a two-dimensional rectangular object, such as
+ * a {@link Sprite}. <br />
+ * <br />
+ * <tt>CollisionShape</tt> instances are required to have a position, height
+ * and width, the ability to set their bounds in numerous ways, and most
+ * importantly, the ability to detect whether or not an
+ * {@link #intersects(CollisionShape) intersection} occurred with another
+ * <tt>CollisionShape</tt> instance. The {@link #intersects(CollisionShape)}
+ * method is a quick way to see if a <tt>CollisionShape</tt> instance has
+ * collided with another instance of <tt>CollisionShape</tt>, however, it is
+ * not necessarily perfect, as the <tt>CollisionShape</tt> instance may
+ * encompass more area than the underlying object represented with its
+ * <tt>CollisionShape</tt> instance.
  * 
- * This interface that play the role to determine whether two sprites are
- * collided to each other or not using <code>CollisionShape</code>
- * {@linkplain #intersects(CollisionShape)} method.
+ * @since 0.2.3
+ * @version 1.1
  */
 public interface CollisionShape {
 	
 	/**
-	 * Returns whether this collision shape intersects with other collision
-	 * shape area.
+	 * Returns true if this <tt>CollisionShape</tt> instance intersects with
+	 * the given <tt>CollisionShape</tt> instance, false otherwise.
+	 * @return True if this <tt>CollisionShape</tt> instance intersects with
+	 *         the given <tt>CollisionShape</tt> instance, false otherwise.
 	 */
 	public boolean intersects(CollisionShape shape);
 	
+	// TODO: refactor this and get rid of the doubles, only use ints for
+	// collision of pixels.
 	/**
-	 * Moves this collision shape to specified location.
+	 * Moves this <tt>CollisionShape</tt> instance to the specified position.
+	 * @param x The {@link #getX() x-coordinate position} of this
+	 *        <tt>CollisionShape</tt> instance.
+	 * @param y The {@link #getY() y-coordinate position} of this
+	 *        <tt>CollisionShape</tt> instance.
 	 */
 	public void setLocation(double x, double y);
 	
+	// TODO: refactor this and get rid of the doubles, only use ints for
+	// collision of pixels.
 	/**
-	 * Moves this collision shape by specified delta.
+	 * Moves this <tt>CollisionShape</tt> instance by the specified offsets.
+	 * @param offsetX The offset to add to this <tt></tt> instance's
+	 *        {@link #getX() x-coordinate position}.
+	 * @param offsetY The offset to add to this <tt></tt> instance's
+	 *        {@link #getY() y-coordinate position}.
 	 */
-	public void move(double dx, double dy);
+	public void move(double offsetX, double offsetY);
 	
+	// TODO: refactor this and get rid of the doubles, only use ints for
+	// collision of pixels.
 	/**
-	 * Sets the boundary of this colllision shape to specified boundary.
+	 * Sets the bounds of this <tt>CollisionShape</tt> instance to the
+	 * specified parameters.
+	 * @param x The {@link #getX() x-coordinate position} of this
+	 *        <tt>CollisionShape</tt> instance.
+	 * @param y The {@link #getY() y-coordinate position} of this
+	 *        <tt>CollisionShape</tt> instance.
+	 * @param width The {@link #getWidth() width} of this
+	 *        <tt>CollisionShape</tt> instance.
+	 * @param height The {@link #getHeight() height} of this
+	 *        <tt>CollisionShape</tt> instance.
 	 */
-	public void setBounds(double x1, double y1, int w1, int h1);
+	public void setBounds(double x, double y, int width, int height);
 	
+	// TODO: refactor this and get rid of this method, replace with a method
+	// returning an integer.
 	/**
-	 * Returns the <code>x</code>-position of this collision shape.
+	 * Returns the <tt>x</tt> coordinate position of this
+	 * <tt>CollisionShape</tt> instance.
+	 * @return The <tt>x</tt> coordinate position of this
+	 *         <tt>CollisionShape</tt> instance.
 	 */
 	public double getX();
 	
+	// TODO: refactor this and get rid of this method, replace with a method
+	// returning an integer.
 	/**
-	 * Returns the <code>y</code>-position of this collision shape.
+	 * Returns the <tt>y</tt> coordinate position of this
+	 * <tt>CollisionShape</tt> instance.
+	 * @return The <tt>y</tt> coordinate position of this
+	 *         <tt>CollisionShape</tt> instance.
 	 */
 	public double getY();
 	
 	/**
-	 * Returns the width of this collision shape.
+	 * Returns the width of this <tt>CollisionShape</tt> instance.
+	 * @return The width of this <tt>CollisionShape</tt> instance.
 	 */
 	public int getWidth();
 	
 	/**
-	 * Returns the height of this collision shape.
+	 * Returns the height of this <tt>CollisionShape</tt> instance.
+	 * @return The height of this <tt>CollisionShape</tt> instance.
 	 */
 	public int getHeight();
 	
